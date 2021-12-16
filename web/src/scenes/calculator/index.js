30c0927ec3145fb836d086b8498b792aae6c793b
Sebastian Reineling <62569847+reineling@users.noreply.github.com>
2021-12-16 19:25:26 +0100

    Update index.js

--- web/src/scenes/calculator/index.js
+++ web/src/scenes/calculator/index.js
@@ -155,15 +155,15 @@ export default class index extends Component {
 
                     <div className={styles.text}>
                         <div className={styles.area}>
-                            <span className={styles.txt}>
-                            <h2 className={`${styles.title} ${styles.t_one}`}>Probability of Earning a Reward</h2>
+                            <span className={`${styles.txt} ${styles.txt2}`}>
+                            <h2 className={`${styles.title} ${styles.t_two}`}>Probability of Earning a Reward</h2>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             </span>
                         </div>
 
                         <div className={styles.area}>
-                            <span className={`${styles.txt} ${styles.txt2}`}>
-                            <h2 className={`${styles.title} ${styles.t_two}`}>Probability of Earning a Reward</h2>
+                            <span className={styles.txt}>
+                            <h2 className={`${styles.title} ${styles.t_one}`}>Probability of Earning a Reward</h2>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             </span>
                         </div>
