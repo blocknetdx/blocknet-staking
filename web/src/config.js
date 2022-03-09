2efdc91c071b197b3703a4c8af23e8e838064415
sh_rn <shrnkld@pm.me>
2022-03-09 11:19:18 +0100

    container update for larger devices

--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     server: {
-        url: 'http://127.0.0.1:3000'
+        //url: 'http://127.0.0.1:3000'
+        //url: 'https://staking.core.cloudchainsinc.com'
+        url: 'https://staking-calc-wt22n.ondigitalocean.app'
     }
 }
